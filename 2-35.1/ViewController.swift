285a7c1f153889208c20d20ce2f843bc308fcd6a
Dmitrii Miroshnichenko <dima@miroshnichenko.pro>
2023-04-18 23:32:16 +0300

    Initial Commit

---
+++ 2-35.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2-35.1
+//
+//  Created by Дмитрий Мирошниченко on 18.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
